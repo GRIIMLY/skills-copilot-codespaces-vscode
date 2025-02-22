6126370f08adf9c9472a92e2fc36066dcdcd2290
GRIIMLY <nicolasmoga12@gmail.com>
2025-02-21 20:54:02 -0500

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,6 @@
+function calculateNumber() {
+    // Calculate the number of skills
+    var skills = document.getElementsByClassName("skill");
+    var number = skills.length;
+    return number;
+}
